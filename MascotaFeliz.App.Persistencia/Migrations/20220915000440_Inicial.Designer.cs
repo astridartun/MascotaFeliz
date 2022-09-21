0241cb9447d3e58bf1b57fce27ae54555dc113ba
astridartun <astrid.artunduaga@gmail.com>
2022-09-20 23:12:57 -0500

    Código 20/09/2022

--- MascotaFeliz.App.Persistencia/Migrations/20220915000440_Inicial.Designer.cs
+++ MascotaFeliz.App.Persistencia/Migrations/20220915000440_Inicial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MascotaFeliz.App.Persistencia.Migrations
 {
     [DbContext(typeof(AppContext))]
-    [Migration("20220828170701_Inicial")]
+    [Migration("20220915000440_Inicial")]
     partial class Inicial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
